f7297bd22b94445afc44dd9655af0b51eafe20cb
SumeiraAkkiuz <akyuz.sumeyragulsum@gmail.com>
2021-02-18 07:23:51 -0500

    navbar added

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,21 +1,87 @@
 import React from 'react';
-import { Button, TextField } from '@material-ui/core';
+import { Button, TextField, Grid, Container } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { useFormik } from 'formik';
+
+const stylesFunc = makeStyles({
+    wrapper: {
+        marginTop: "5rem",
+    }
+});
 
 function Signup() {
-    return (
-        <div>
-            <TextField id="outlined-basic" label="Display Name" variant="outlined" />
-            <TextField id="outlined-basic" label="Email" variant="outlined" />
-            <TextField id="outlined-basic" label="Password" variant="outlined" />
+    const formik = useFormik({
+        initialValues: {
+            displayName: "",
+            email: "",
+            password: "",
+        },
+        onSubmit: (values)=>{
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
 
-            <Button variant="contained" color="primary">
-                Submit
-            </Button>
-            <Button variant="contained" color="primary">
-                SignUp with Google
-            </Button>
-           
-        </div>
+    const signUpStyle = stylesFunc();
+    return (
+        <Container className={signUpStyle.wrapper} maxWidth="sm">
+            <form onSubmit={formik.handleSubmit}>
+            <Grid container spacing={3}>
+               
+                <Grid item xs={12}>
+                    <TextField 
+                    name="displayName"
+                    label="Display Name" 
+                    variant="outlined" 
+                    fullWidth 
+                    value={formik.values.displayName} 
+                    onChange={formik.handleChange} 
+                    />
+                </Grid> 
+
+                <Grid item xs={12}>      
+                    <TextField 
+                    name="email"
+                    label="Email" 
+                    variant="outlined" 
+                    fullWidth
+                    value={formik.values.email} 
+                    onChange={formik.handleChange}
+                    />
+                </Grid>     
+
+                <Grid item xs={12}>
+                    <TextField 
+                    name="password"
+                    label="Password" 
+                    variant="outlined" 
+                    type="password"
+                    fullWidth
+                    value={formik.values.password} 
+                    onChange={formik.handleChange}
+                    />
+                </Grid> 
+
+                <Grid item xs={12}>
+                    <Button type="submit"
+                    variant="contained" 
+                    color="primary" 
+                    fullWidth>
+                    Submit
+                    </Button>
+                </Grid> 
+
+                <Grid item xs={12}>
+                    <Button 
+                    variant="contained" 
+                    color="primary" 
+                    fullWidth>
+                    SignUp with Google
+                    </Button>
+                </Grid> 
+
+            </Grid>
+            </form>
+        </Container>
     )
 }
 
